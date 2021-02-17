795cd4450d97f7513db79ff6d854a1cc24fbb7fb
Joha Kim <johakim9936@gmail.com>
2021-02-17 13:36:00 -0500

    Render users in GetUsers component's UI

--- client/src/Components/GetUsers.js
+++ client/src/Components/GetUsers.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery, gql } from "@apollo/client";
 import { LOAD_USERS } from "../GraphQL/Queries";
 
 function GetUsers() {
   const { loading, error, data } = useQuery(LOAD_USERS);
+  const [users, setUsers] = useState([]);
 
   // component re-rendered whenever there are changes to users
   useEffect(() => {
-    console.log(data);
+    if (data) {
+      console.log(data);
+      setUsers(data.getAllUsers.slice(0, 4));
+    }
   }, [data]);
 
   if (loading) return "Loading...";
@@ -15,6 +19,13 @@ function GetUsers() {
 
   return (
     <div>
+      {users.map((user, index) => {
+        return (
+          <h1 key={index}>
+            {user.firstName} {user.lastName}
+          </h1>
+        );
+      })}
     </div>
   );
 }
